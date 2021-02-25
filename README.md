4c908722b405d2af8d213a2a088dc4e3c55e954c
Nico De Witte <nico.dewitte@outlook.com>
2021-02-25 10:13:31 +0100

    Demo of WPF menu

---
+++ README.md
@@ -0,0 +1,11 @@
+# Demo of WPF Menu
+
+Demo app with Menu.
+
+## How to get this locally
+
+Go to a local directory on your system and open Powershell in that directory. Now clone this repo using git:
+
+```bash
+git clone git@github.com:vives-introduction-to-programming-2020/WPFMenuDemo.git
+```
---
+++ WPFMenuDemo/MenuWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMenuDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void about_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Someone clicked the about button");
+
+            AboutWindow about = new AboutWindow();
+            about.Show();
+
+            this.Close();
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void userInput_Click(object sender, RoutedEventArgs e)
+        {
+            UserInput input = new UserInput();
+            input.Show();
+
+            this.Close();
+        }
+    }
+}
